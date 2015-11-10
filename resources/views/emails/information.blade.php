8c59f2a8fb19615825f007a79b746600aeed8177
marsflow <stevanadjie@gmail.com>
2015-11-10 10:14:26 +0700

    Registration Fix

--- resources/views/emails/information.blade.php
+++ resources/views/emails/information.blade.php
@@ -3,10 +3,17 @@
 
 	Dear {{ $user->name }},
 
-	 @if (env('APP_ENV') == 'local')
+	 @if (env('APP_ENV') == 'production')
 
 		<p>Untuk berpartisipasi dalam SMPI (Sistem Penjamin Mutu Internal), 
-		Authentikasi dibutuhkan silahkan klik <a href="http://weblogindonesia.com/register/confirm/{{ $token }}">link berikut</a>, atau copy link berkut <a href="http://localhost:8000/register/confirm/{{ $token }}">http://localhost:8000/register/confirm/{{ $token }}</a> pada url browser anda</p>
+		Authentikasi dibutuhkan silahkan klik <a href="http://weblogindonesia.com/register/confirm/{{ $token }}">link berikut</a>, atau copy link berkut <a href="http://weblogindonesia.com/register/confirm/{{ $token }}">http://weblogindonesia.com/register/confirm/{{ $token }}</a> pada url browser anda</p>
+
+	@endif
+	
+	@if (env('APP_ENV') == 'local')
+
+		<p>Untuk berpartisipasi dalam SMPI (Sistem Penjamin Mutu Internal), 
+		Authentikasi dibutuhkan silahkan klik <a href="http://weblogindonesia.com/register/confirm/{{ $token }}">link berikut</a>, atau copy link berkut <a href="http://weblogindonesia.com/register/confirm/{{ $token }}">http://weblogindonesia.com/register/confirm/{{ $token }}</a> pada url browser anda</p>
 
 	@endif
 </div>
